c6ffd06ee47e146eb8313d217030e401c585ee74
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-05-14 20:05:29 -0300

    Initial commit

---
+++ src/main/java/com/eclesiastelucientech/com/servicespringbootreact/ServiceSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.eclesiastelucientech.com.servicespringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSpringBootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eclesiastelucientech/com/servicespringbootreact/ServiceSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclesiastelucientech.com.servicespringbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
